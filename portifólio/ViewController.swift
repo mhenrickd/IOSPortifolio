ccb1b8ca5c1f3487bd91cc19ce19522b447822e8
Matheus Henrick Dias <mhenrickd@gmail.com>
2020-03-19 16:46:46 -0300

    initial commit

--- portifólio/ViewController.swift
+++ portifólio/ViewController.swift
@@ -7,12 +7,51 @@
 //
 
 import UIKit
+import iCarousel
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, iCarouselDataSource, iCarouselDelegate {
 
+    var quadros = ["photo1","photo2","photo3","photo4"]
+    @IBOutlet weak var carousel: iCarousel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        carousel.type = .rotary
+        carousel.dataSource = self
+        carousel.delegate = self
+        carousel.autoscroll = 1
+        
+    }
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return quadros.count
+    }
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        let imagem: UIImageView
+        
+        if let view = view as? UIImageView{
+            //ja existe uma view q pode ser reutilizada
+            imagem = view
+        } else {
+            imagem = UIImageView()
+            imagem.frame = carousel.frame
+            imagem.contentMode = .scaleAspectFill
+            imagem.clipsToBounds = true
+            imagem.layer.cornerRadius = 50
+        }
+        imagem.image = UIImage(named: quadros[index])
+        return imagem
+    }
+    
+    func carousel(_ carousel: iCarousel, valueFor option: iCarouselOption, withDefault value: CGFloat) -> CGFloat {
+        
+        switch option {
+        case .spacing:
+            return value * 1.2
+        default:
+            return value
+        }
     }
 
 
